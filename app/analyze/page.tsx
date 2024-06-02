e10c4e5a9f491108cc6eef3808e87aa4641a68f4
priyesh pandey <144878884+PriyeshPandey2000@users.noreply.github.com>
2024-06-02 08:19:03 +0000

    improved ui

--- app/analyze/page.tsx
+++ app/analyze/page.tsx
@@ -27,7 +27,8 @@ const Questions: React.FC<QuestionsProps> = ({ questions, onSubmit }) => {
     event.preventDefault();
     const formattedAnswers = Object.fromEntries(
       Object.entries(answers).map(([id, value]) => [
-        questions.find((q) => q.id === parseInt(id))?.text ?? 'Unknown Question',
+        questions.find((q) => q.id === parseInt(id))?.text ??
+          'Unknown Question',
         value
       ])
     );
@@ -38,15 +39,17 @@ const Questions: React.FC<QuestionsProps> = ({ questions, onSubmit }) => {
     <form onSubmit={handleSubmit} className="max-w-3xl mx-auto">
       {questions.map((question) => (
         <div key={question.id} className="mb-6">
-          <label className="block mb-2">{question.text}</label>
+          <label className="block mb-2 text-bold">{question.text}</label>
           {question.type === 'dropdown' && question.options ? (
-            <div className="flex flex-wrap -mx-2">
+            <div className="flex flex-wrap -mx-2 text-semibold">
               {question.options.map((option, index) => (
                 <div className="flex-1 px-2 mb-4 sm:mb-3 sm:flex-none sm:w-1/2 lg:w-1/4 xl:w-1/4">
                   <label
                     key={index}
-                    className={`w-full border p-2 rounded cursor-pointer ${
-                      answers[question.id] === option.label ? 'bg-blue-100' : 'bg-white'
+                    className={`w-full border p-2 rounded cursor-pointer bg-yellow-200 ${
+                      answers[question.id] === option.label
+                        ? 'bg-blue-100 border-black border-4'
+                        : 'bg-white border-black border-3'
                     } flex flex-col items-center space-y-2`}
                   >
                     <input
@@ -54,10 +57,14 @@ const Questions: React.FC<QuestionsProps> = ({ questions, onSubmit }) => {
                       name={`question-${question.id}`}
                       value={option.label}
                       checked={answers[question.id] === option.label}
-                      onChange={(e) => handleChange(question.id, e.target.value)}
+                      onChange={(e) =>
+                        handleChange(question.id, e.target.value)
+                      }
                       className="hidden"
                     />
-                    {option.icon && <span className="icon text-xl">{option.icon}</span>}
+                    {option.icon && (
+                      <span className="icon text-xl">{option.icon}</span>
+                    )}
                     <span className="pb-1 px-2">{option.label}</span>
                   </label>
                 </div>
@@ -68,12 +75,15 @@ const Questions: React.FC<QuestionsProps> = ({ questions, onSubmit }) => {
               value={answers[question.id] || ''}
               onChange={(e) => handleChange(question.id, e.target.value)}
               placeholder="Your answer"
-              className="block w-full border border-gray-300 rounded px-3 py-2"
+              className="block w-full border border-gray-300 rounded px-3 py-2 "
             />
           )}
         </div>
       ))}
-      <button type="submit" className="bg-blue-500 text-white font-bold py-2 px-4 rounded">
+      <button
+        type="submit"
+        className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+      >
         Submit
       </button>
     </form>
@@ -111,7 +121,7 @@ export default function CombinedPage() {
         if (reader.result && typeof reader.result === 'string') {
           result = await generateText(reader.result, JSON.stringify(answers));
           setGeneratedText(result);
-          const imagePrompt = JSON.parse(result)["outfit_image_prompt"];
+          const imagePrompt = JSON.parse(result)['outfit_image_prompt'];
           const imageUrl = await generateImage(imagePrompt, answers);
           setImageUrl(imageUrl);
         }
@@ -119,7 +129,7 @@ export default function CombinedPage() {
     } else {
       result = await generateText(null, answers);
       setGeneratedText(result);
-      const imagePrompt = JSON.parse(result)["outfit_image_prompt"];
+      const imagePrompt = JSON.parse(result)['outfit_image_prompt'];
       const imageUrl = await generateImage(imagePrompt);
       setImageUrl(imageUrl);
     }
@@ -132,18 +142,30 @@ export default function CombinedPage() {
   };
 
   return (
-    <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6">
+    <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-6 bg-gradient-to-br from-green-300 to-blue-400 justify-center items-center">
       <div className="flex items-center">
-        <h1 className="font-semibold text-lg md:text-2xl">Analyze Image</h1>
+        <h1 className="font-semibold text-lg md:text-4xl">Analyze Image</h1>
       </div>
       {showForm && (
         <div>
           <div className="w-full flex items-center mb-5 max-w-3xl mx-auto">
-            {image && <img src={URL.createObjectURL(image)} alt="Uploaded image" className="max-w-32 p-4" />}
+            {image && (
+              <img
+                src={URL.createObjectURL(image)}
+                alt="Uploaded image"
+                className="max-w-32 p-4"
+              />
+            )}
             <Input id="picture" type="file" onChange={handleImageUpload} />
           </div>
           <div className="flex flex-col items-center">
-            <Questions questions={questions} onSubmit={(answers) => { setAnswers(answers); handleSubmit() }} />
+            <Questions
+              questions={questions}
+              onSubmit={(answers) => {
+                setAnswers(answers);
+                handleSubmit();
+              }}
+            />
           </div>
         </div>
       )}
@@ -167,7 +189,11 @@ export default function CombinedPage() {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         {imageUrl && (
           <div className="w-full mb-4 text-center relative">
-            <img src={imageUrl} alt="Generated" className="border rounded-sm border-input" />
+            <img
+              src={imageUrl}
+              alt="Generated"
+              className="border rounded-sm border-input"
+            />
           </div>
         )}
         {imageLoading && (
@@ -181,18 +207,18 @@ export default function CombinedPage() {
         )}
       </div>
       <div className="flex gap-6">
-      {generatedText && (
-              <table>
-              <tbody>
-                {Object.entries(JSON.parse(generatedText)).map(([key, value]) => (
-                  <tr key={key}>
-                    <td>{key}</td>
-                    <td>{"" + value}</td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>      
-      )}
+        {generatedText && (
+          <table>
+            <tbody>
+              {Object.entries(JSON.parse(generatedText)).map(([key, value]) => (
+                <tr key={key}>
+                  <td>{key}</td>
+                  <td>{'' + value}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </main>
   );
